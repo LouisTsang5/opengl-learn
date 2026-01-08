49ef8171f058237e295c77c23e63a1ccc1d6bfff
Louis Tsang <louistsang5@gmail.com>
2026-01-08 16:45:38 +0800

    make a window

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <unistd.h>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-#define errExit(str) { \
-    puts(str); \
-    exit(-1); \
-}
+#define WIDTH 800
+#define HEIGHT 600
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void process_input(GLFWwindow *window);
 
 int main() {
     puts("GLFW Test");
@@ -19,21 +21,49 @@ int main() {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // Make window
-    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", NULL, NULL);
     if (window == NULL) {
-        glfwTerminate();
-        errExit("Failed to create GLFW window");
+        puts("Failed to create GLFW window");
+        goto exit_err;
     }
     glfwMakeContextCurrent(window);
 
     // Init glad
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        glfwTerminate();
-        errExit("Failed to initialize GLAD");
+        puts("Failed to initialize GLAD");
+        goto exit_err;
     }
 
-    // Loop
-    while(1) {
-        sleep(10);
+    // Set view port
+    glViewport(0, 0, WIDTH, HEIGHT);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // Main loop
+    while (!glfwWindowShouldClose(window)) {
+        process_input(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
     }
+    
+    // Terminate
+    glfwTerminate();
+    return 0;
+
+    exit_err:
+    glfwTerminate();
+    return -1;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    printf("New dimensions - width: %d, height: %d\n", width, height);
+    glViewport(0,0,width, height);
+}
+
+void process_input(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
 }
